cdfa41c4cee1873b8a9156c4b61e0d13c83fdcaf
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-07-22 11:04:44 -0700

    Fixed landing page img and footer

--- models/models.js
+++ models/models.js
@@ -5,15 +5,29 @@ salt = bcrypt.genSaltSync(10);
 
 // SCHEMAS
 
-var validateUser = function() {
-	// return true or false
-}
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+
 
 var UserSchema = new Schema({
-	name: String,
-	email: String,
-	passwordDigest: String,
-	// validate: validateUser
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: 'Email address is required',
+		validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+	},
+	passwordDigest: {
+		type: String,
+		required: true
+	}
 });
 
 var PhotoSchema = new Schema({
