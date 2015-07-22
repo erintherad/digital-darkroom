20030b9aa20759ba4003fdd90e61480b543c0144
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-07-21 19:34:00 -0700

    Fixed login modal

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,8 +2,7 @@ var originalCaman,
 	filterContainer = $('#filterContainer');
 
 $(document).ready(function() {
-	$('#signInModal').on('show', 'toggle', function(event) {
-	  event.preventDefault();
-	  $('#email').focus();
+	$('#signInModal').on('shown.bs.modal', function(event) {
+		$('#email').focus();
 	});
 });
