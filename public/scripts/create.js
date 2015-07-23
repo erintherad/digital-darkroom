574d49c1b6d38d2fab8e9e658fa2e52083943d3f
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-07-23 16:35:31 -0700

    push to heroku

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -131,8 +131,8 @@ $(document).ready(function() {
 			},
 			success: function(data) {
 				alert('successfully created photo!');
-				// window.location = "https://digital-darkroom.herokuapp.com/gallery";
-				window.location = "http://localhost:3000/gallery";
+				window.location = "https://digital-darkroom.herokuapp.com/gallery";
+				// window.location = "http://localhost:3000/gallery";
 			}
 		});
 	});
